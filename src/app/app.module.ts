93656152308d2784bc4238c3940e246f9bb43890
nikhil gatla <62739007+nikhilgat@users.noreply.github.com>
2022-07-27 15:25:01 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
@@ -23,6 +22,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     LoginComponent,
     RegisterComponent,
     NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
