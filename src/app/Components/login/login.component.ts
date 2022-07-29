4f10f0953f22cdd089f1a502ccbc2ee995be9cf4
nikhil gatla <62739007+nikhilgat@users.noreply.github.com>
2022-07-29 08:55:44 +0530

    commit

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -8,9 +8,6 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   hide = true;
-
-  
-
   email = new FormControl('', [Validators.required, Validators.email]);
   getErrorMessage() {
     if (this.email.hasError('required')) {
--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'register',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 
   constructor() { }
 
