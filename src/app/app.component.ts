c884253b64d0e2a964daafabdba40af67ac2577c
nikhil gatla <62739007+nikhilgat@users.noreply.github.com>
2022-08-10 10:25:00 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movieApp';
+
+
+  // constructor(private dialogRef : MatDialog ){}
+
+  // openDialog(){
+  //   this.dialogRef.open(AddreviewComponent)
+  // }
+
 }
