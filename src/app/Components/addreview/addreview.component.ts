e9a6977977f4ea47f10a5935121788642416bcf2
nikhil gatla <62739007+nikhilgat@users.noreply.github.com>
2022-08-14 02:12:46 +0530

    commit

---
+++ src/app/Components/addreview/addreview.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addreview',
+  templateUrl: './addreview.component.html',
+  styleUrls: ['./addreview.component.css']
+})
+export class AddreviewComponent implements OnInit {
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {}
+  posts: any[] = [];
+
+ editpost:edit={id:0 ,title:"",description:"",review:"", imageUrl:""}
+
+  loadposts() {
+    this.http.get('http://localhost:8080/getposts').subscribe((posts: any) => {
+      this.posts = posts;
+    });
+  }
+  uploadposts() {
+    if(this.editpost.title.length>0 && this.editpost.description.length>0 && this.editpost.imageUrl.length>0)
+    {
+      this.http.post('http://localhost:8080/postposts', this.editpost).subscribe(
+        (res) => {
+          alert('Posted Successfully');
+          location.reload;
+          console.log(this.editpost);
+        },
+      );
+    }
+  else{
+    alert('Error has occured please enter all the details');
+  }
+  }
+}
+export interface edit{
+  id:number
+  title:string
+  description:string
+  review:string
+  imageUrl:string
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddreviewComponent } from './addreview/addreview.component';
+import { AddreviewComponent } from './Components/addreview/addreview.component';
 import { CatalogueComponent } from './Components/catalogue/catalogue.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
@@ -8,13 +8,13 @@ import { RegisterComponent } from './Components/register/register.component';
 import { TestComponent } from './Components/test/test.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent },
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'catalogue', component: CatalogueComponent},
-  {path: 'test', component: TestComponent },
-  {path: 'addreview', component: AddreviewComponent }
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'catalogue', component: CatalogueComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'addreview', component: AddreviewComponent }
 
 
 
